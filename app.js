7b3932a9458bf9d56db5b2ee4e939606c0b25b25
Hannes Roth <hannes.roth88@gmail.com>
2018-10-14 19:55:56 +0200

    update

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var exec = require('child_process').exec, child;
 
 
 
+
 child = exec('hyperion-remote -l',
 function (error, stdout, stderr) {
     //console.log('stdout: ' + stdout);
@@ -13,19 +14,19 @@ function (error, stdout, stderr) {
     
 
 
-
     index_start = stdout.lastIndexOf('"RGB Value" : [ ');
     stdout = stdout.slice(index_start+16);
     index_end = stdout.indexOf(' ]');
     stdout = stdout.slice(0,index_end);
     var arrayRGB = stdout.split(', ');
     console.log("LED RGB Code " + arrayRGB);
-    colorRed = arrayRGB[0];
-    colorGreen = arrayRGB[1];
-    colorBlue = arrayRGB[2];
+    colorRed = arrayRGB[0]/2.55;
+    colorGreen = arrayRGB[1]/2.55;
+    colorBlue = arrayRGB[2]/2.55;
 
 
-    var xhttp = new XMLHttpRequest();
+
+var xhttp = new XMLHttpRequest();
     console.log("Send Colors over REST API");
     xhttp.onreadystatechange = function() {
             if (this.readyState == 1 && this.status == 0 ) {
@@ -33,16 +34,17 @@ function (error, stdout, stderr) {
                 console.log("success: responseText " + this.responseText);
             }
     };
-    xhttp.open("PUT", "http://192.168.2.4:8080/rest/items/EG_Wohnen_LedR/state", true); // 0-100
+    xhttp.open("PUT", "http://192.168.2.4:8080/rest/items/EG_Wohnen_LedR/state", false); // 0-100
     xhttp.setRequestHeader("Content-type", "text/plain");
     xhttp.send(colorRed);
-    xhttp.open("PUT", "http://192.168.2.4:8080/rest/items/EG_Wohnen_LedG/state", true); // 0-100
-    xhttp.setRequestHeader("Content-type", "text/plain");
+
+    xhttp.open("PUT", "http://192.168.2.4:8080/rest/items/EG_Wohnen_LedG/state", false); // 0-100
     xhttp.send(colorGreen);
-    xhttp.open("PUT", "http://192.168.2.4:8080/rest/items/EG_Wohnen_LedB/state", true); // 0-100
-    xhttp.setRequestHeader("Content-type", "text/plain");
+
+    xhttp.open("PUT", "http://192.168.2.4:8080/rest/items/EG_Wohnen_LedB/state", false); // 0-100
     xhttp.send(colorBlue);
     
+
 });
 
 //console.log("responseText " + xhttp.responseText);
