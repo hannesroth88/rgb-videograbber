b9141d850fe80b0b0ec3275ea4e059e959c2f520
Hannes Roth <hannes.roth88@gmail.com>
2018-10-14 17:51:30 +0200

    update

--- app.js
+++ app.js
@@ -3,31 +3,38 @@ var exec = require('child_process').exec, child;
 
     child = exec('hyperion-remote -l',
     function (error, stdout, stderr) {
-        console.log('stdout: ' + stdout);
+        //console.log('stdout: ' + stdout);
         console.log('stderr: ' + stderr);
         if (error !== null) {
              console.log('exec error: ' + error);
         }        
-        let obj = JSON.parse(stdout);
+        let obj = stdout.slice(1, str.indexOf('Server info:'))
+        console.log("whole JSON " + obj)
+        obj = JSON.parse(obj);
         console.log("LED RGB Code " + obj.activeLedColor)
+
+
+
+
+        var xhttp = new XMLHttpRequest();
+        console.log("Send Colors over REST API");
+        xhttp.onreadystatechange = function() {
+                if (this.readyState == 1 && this.status == 0 ) {
+                    //alert(this.responseText);
+                    console.log("success: responseText " + this.responseText);
+                }
+        };
+        xhttp.open("PUT", "http://192.168.2.4:8080/rest/items/EG_Wohnen_LedR/state", true); // 0-100
+        xhttp.setRequestHeader("Content-type", "text/plain");
+        xhttp.send("100");
+
     });
 
-    //let birth = '{ "birthDate":"1993-09-10"}';
 
 
 
 
-    var xhttp = new XMLHttpRequest();
-    console.log("Send Colors over REST API");
-    xhttp.onreadystatechange = function() {
-            if (this.readyState == 1 && this.status == 0 ) {
-                //alert(this.responseText);
-                console.log("success: responseText " + this.responseText);
-            }
-    };
-    xhttp.open("PUT", "http://192.168.2.4:8080/rest/items/EG_Wohnen_LedR/state", true); // 0-100
-    xhttp.setRequestHeader("Content-type", "text/plain");
-    xhttp.send("100");
+
     //console.log("responseText " + xhttp.responseText);
     //console.log("status " + xhttp.status);
     //console.log("readyState " + xhttp.readyState);    
